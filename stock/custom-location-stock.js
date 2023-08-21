f98b4323dd4ee252fdca1f3a1136654ea408b5eb
Naseem Abbas <120629585+naseemabbas14@users.noreply.github.com>
2023-08-21 18:04:49 +0500

    CTI-2214
    https://pms.vaivaltech.com/browse/CTI-2214

--- stock/custom-location-stock.js
+++ stock/custom-location-stock.js
@@ -467,6 +467,7 @@ scriptInjection("https://code.jquery.com/jquery-3.2.1.min.js", function () {
         var meta = { "product": { "id": __st.rid } };
     }
     
+    
     /**
      * check the status of cart page
      */
