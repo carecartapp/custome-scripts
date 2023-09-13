9286aa3b0cc60f616182baa0e961852c6304da35
Naseem Abbas <120629585+naseemabbas14@users.noreply.github.com>
2023-09-13 17:25:15 +0500

    CTI-2318
    https://pms.vaivaltech.com/browse/CTI-2318

--- spinner/new-ui-careCartAppSpinner.js
+++ spinner/new-ui-careCartAppSpinner.js
@@ -1,5 +1,5 @@
 //******* @author: CareCart App-Wheelify - Rehan Azaz *******************************************
-//****** Store Frontend JS - carecartSpinnerApp.js GH v.6.0.0 - Build ver 2.86 *******************
+//****** Store Frontend JS - carecartSpinnerApp.js GH v.6.0.0 - Build ver 3.26 *******************
 //****** Updated at: 18-Feb-2022, 11:52 AM  ********************************************************
 
 (function () {
@@ -3245,7 +3245,7 @@
                 }
 
                 if (Shopify.shop == 'sirplus-dev.myshopify.com') {
-                    carecartSpinnerJquery('head').append('<style type="text/css">.wheelify-signupContainer .checkbox{width: auto;height: auto;background: transparent;border: none;}</style>');
+                    carecartSpinnerJquery('head').append('<style type="text/css">.wheelify-signupContainer .checkbox{width: auto;height: auto;background: transparent;border: none;}#wheelify-spin_a_sale_cc_store_front_module .btn-submit-form, #wheelify-spin_a_sale_cc_store_front_module .btn-submit-form-ok, #wheelify-spin_a_sale_cc_store_front_module .copy-button{text-align:center;}</style>');
                 }
 
                 if (Shopify.shop == 'banzaihobby.myshopify.com') {
