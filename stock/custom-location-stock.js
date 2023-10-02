6ea1ef8760e25f4e73cf73f90edb914ce68dd7e5
Naseem Abbas <120629585+naseemabbas14@users.noreply.github.com>
2023-10-02 18:51:18 +0500

    CTI-2383
    https://pms.vaivaltech.com/browse/CTI-2383

--- stock/custom-location-stock.js
+++ stock/custom-location-stock.js
@@ -3,7 +3,7 @@
  * @author CareCart
  * @link https://apps.shopify.com/partners/care-cart
  * @link https://carecart.io/
- * @version 3.45
+ * @version 3.57
  *
  * Any unauthorized use and distribution of this and related files, is strictly forbidden.
  * In case of any inquiries, please contact here: https://carecart.io/contact-us/
@@ -485,6 +485,9 @@ scriptInjection("https://code.jquery.com/jquery-3.2.1.min.js", function () {
         var meta = { "product": { "id": __st.rid } };
     }
     else if (Shopify.shop == "gosunnah-com.myshopify.com") {
+        var meta = { "product": { "id": __st.rid } };
+    }
+    else if (Shopify.shop == "794f78.myshopify.com") {
         var meta = { "product": { "id": __st.rid } };
     }
     else{
