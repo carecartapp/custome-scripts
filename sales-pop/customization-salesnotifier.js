71f735537a38efed5c232cbd920fca34af879069
dilshadahmed838 <111492848+dilshadahmed838@users.noreply.github.com>
2023-10-03 18:15:16 +0500

    version changed
    version changed

--- sales-pop/customization-salesnotifier.js
+++ sales-pop/customization-salesnotifier.js
@@ -3,7 +3,7 @@
  * @author CareCart
  * @link https://apps.shopify.com/partners/care-cart
  * @link https://carecart.io/
- * @version 3.61
+ * @version 3.65
  *
  * Any unauthorized use and distribution of this and related files, is strictly forbidden.
  * In case of any inquiries, please contact here: https://carecart.io/contact-us/
