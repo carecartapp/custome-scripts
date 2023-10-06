7abc6a2d6b8d7782625772855b057057e31ee381
dilshadahmed838 <111492848+dilshadahmed838@users.noreply.github.com>
2023-10-06 12:20:44 +0500

    CTI-2403
    https://pms.vaivaltech.com/browse/CTI-2403

--- visitor/custom-location-visitor.js
+++ visitor/custom-location-visitor.js
@@ -3,7 +3,7 @@
  * @author CareCart
  * @link https://apps.shopify.com/partners/care-cart
  * @link https://carecart.io/
- * @version 3.80
+ * @version 3.82
  *
  * Any unauthorized use and distribution of this and related files, is strictly forbidden.
  * In case of any inquiries, please contact here: https://carecart.io/contact-us/
@@ -1575,7 +1575,7 @@ scriptInjection("https://code.jquery.com/jquery-3.2.1.min.js", function () {
     
     function visitorCounter(responseVisitor) {
        var selectorVisitor1 = $jq321("form[action='/cart/add']").find("button[type='submit'],input[type='submit']").parent();
-        var selectorVisitor2 = $jq321("form[action='/cart/add']");
+        //var selectorVisitor2 = $jq321("form[action='/cart/add']");
         var selectorVisitor3 = $jq321("form[action='/cart/add']:first").find("button[type='submit'],input[type='submit']");
         var selectorVisitor4 = $jq321("form[action='/cart/add']:first");
 
@@ -1588,9 +1588,10 @@ scriptInjection("https://code.jquery.com/jquery-3.2.1.min.js", function () {
              }
              
              
-            else if (selectorVisitor2.length == 1) {
+            /*else if (selectorVisitor2.length == 1) {
                 selectorVisitor2.prepend(responseVisitor.view);
             }
+            */
             else if (selectorVisitor3.length == 1) {
                 $jq321(responseVisitor.view).insertBefore(selectorVisitor3);
             }
