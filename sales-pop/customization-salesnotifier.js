939cb815fd32fe9cd4a308565c216b6a5bdf55bd
Naseem Abbas <120629585+naseemabbas14@users.noreply.github.com>
2023-10-17 11:56:00 +0500

    CTI-2460
    https://pms.vaivaltech.com/browse/CTI-2460

--- sales-pop/customization-salesnotifier.js
+++ sales-pop/customization-salesnotifier.js
@@ -3,7 +3,7 @@
  * @author CareCart
  * @link https://apps.shopify.com/partners/care-cart
  * @link https://carecart.io/
- * @version 4.2
+ * @version 4.11
  *
  * Any unauthorized use and distribution of this and related files, is strictly forbidden.
  * In case of any inquiries, please contact here: https://carecart.io/contact-us/
@@ -3314,6 +3314,11 @@ scriptInjection("https://code.jquery.com/jquery-3.2.1.min.js", function () {
         if (Shopify.shop == "b3fc1c.myshopify.com") {
             $jq321("head").append('<style type="text/css">.stock-top{display: block !important;}</style>');
             masterSelector = $jq321(".shopify-block.shopify-app-block");
+            finalSelector = masterSelector[0];
+        }
+        if (Shopify.shop == "soothesole-australia.myshopify.com") {
+            $jq321("head").append('<style type="text/css">.stock-top{display: block !important;}</style>');
+            masterSelector = $jq321(".ast-vd-options");
             finalSelector = masterSelector[0];
         }
 
