b4b6e6d789d7824c7d00a83c76fec1e624c3dfa8
dilshadahmed838 <111492848+dilshadahmed838@users.noreply.github.com>
2023-10-26 17:17:43 +0500

    version update
    version update

--- visitor/custom-location-visitor.js
+++ visitor/custom-location-visitor.js
@@ -3,7 +3,7 @@
  * @author CareCart
  * @link https://apps.shopify.com/partners/care-cart
  * @link https://carecart.io/
- * @version 4.33
+ * @version 4.34
  *
  * Any unauthorized use and distribution of this and related files, is strictly forbidden.
  * In case of any inquiries, please contact here: https://carecart.io/contact-us/
