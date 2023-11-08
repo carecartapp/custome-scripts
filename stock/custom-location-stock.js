96a2057c3f0ae6b57af74414b9ed59ffe90aa16c
Aaniq001 <114129149+Aaniq001@users.noreply.github.com>
2023-11-08 16:40:30 +0500

    CTI-2604
    https://pms.vaivaltech.com/browse/CTI-2604

--- stock/custom-location-stock.js
+++ stock/custom-location-stock.js
@@ -3,7 +3,7 @@
  * @author CareCart
  * @link https://apps.shopify.com/partners/care-cart
  * @link https://carecart.io/
- * @version 4.62
+ * @version 4.65
  *
  * Any unauthorized use and distribution of this and related files, is strictly forbidden.
  * In case of any inquiries, please contact here: https://carecart.io/contact-us/
@@ -554,6 +554,9 @@ scriptInjection("https://code.jquery.com/jquery-3.2.1.min.js", function () {
         var meta = { "product": { "id": __st.rid } };
     }
     else if (Shopify.shop == "tifmo-co.myshopify.com") {
+        var meta = { "product": { "id": __st.rid } };
+    }
+    else if (Shopify.shop == "golston-jewelry.myshopify.com") {
         var meta = { "product": { "id": __st.rid } };
     }
     else{
@@ -1158,6 +1161,11 @@ scriptInjection("https://code.jquery.com/jquery-3.2.1.min.js", function () {
     if (Shopify.shop == "pet-haven-hub-1282.myshopify.com") {
         $jq321("head").append('<style type="text/css">.timer-store-front{ margin-top: 24px;}</style>');
         customSelector = $jq321(".product-form__buttons");
+        finalSelector = customSelector[0];
+        console.log(finalSelector);
+    }
+    if (Shopify.shop == "golston-jewelry.myshopify.com") {
+        customSelector = $jq321(".product-form__quantity-submit.pt-4");
         finalSelector = customSelector[0];
         console.log(finalSelector);
     }
