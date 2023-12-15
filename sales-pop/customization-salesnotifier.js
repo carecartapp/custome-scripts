667efecc97c15955b26995215645f1f1ebb58a31
Aaniq001 <114129149+Aaniq001@users.noreply.github.com>
2023-12-15 17:08:47 +0500

    CTI-2881
    https://pms.vaivaltech.com/browse/CTI-2881

--- sales-pop/customization-salesnotifier.js
+++ sales-pop/customization-salesnotifier.js
@@ -3,7 +3,7 @@
  * @author CareCart
  * @link https://apps.shopify.com/partners/care-cart
  * @link https://carecart.io/
- * @version 5.57
+ * @version 5.58
  *
  * Any unauthorized use and distribution of this and related files, is strictly forbidden.
  * In case of any inquiries, please contact here: https://carecart.io/contact-us/
@@ -3107,6 +3107,11 @@ scriptInjection("https://code.jquery.com/jquery-3.2.1.min.js", function () {
     if (Shopify.shop == "7262dc-2.myshopify.com") {
         $jq321("head").append('<style type="text/css">.stock-top{display: block !important; margin-top:10px !important;}</style>');
         masterSelector = $jq321(".buy-buttons.buy-buttons--multiple");
+        finalSelector = masterSelector[0];
+    }
+    if (Shopify.shop == "deastilosamilano.myshopify.com") {
+        $jq321("head").append('<style type="text/css">.stock-top{display: block !important; margin-top:10px !important;}</style>');
+        masterSelector = $jq321(".product-form__buttons");
         finalSelector = masterSelector[0];
     }
     /** Stock for variants **/
