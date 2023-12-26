d91b49e44198c5e2880304b76db3decaab796b74
Naseem Abbas <120629585+naseemabbas14@users.noreply.github.com>
2023-12-26 16:48:01 +0500

    CTI-2919
    CTI-2919

--- stock/custom-location-stock.js
+++ stock/custom-location-stock.js
@@ -3,7 +3,7 @@
  * @author CareCart
  * @link https://apps.shopify.com/partners/care-cart
  * @link https://carecart.io/
- * @version 5.71
+ * @version 5.75
  *
  * Any unauthorized use and distribution of this and related files, is strictly forbidden.
  * In case of any inquiries, please contact here: https://carecart.io/contact-us/
@@ -1282,6 +1282,10 @@ scriptInjection("https://code.jquery.com/jquery-3.2.1.min.js", function () {
         customSelector = $jq321(".product-form__buttons");
         finalSelector = customSelector[0];
     }
+    if (Shopify.shop == "6bccd5.myshopify.com") {
+        customSelector = $jq321("._rsi-buy-now-button");
+        finalSelector = customSelector[0];
+    }
     
     function stockCountdown(response) {
 
@@ -1368,7 +1372,11 @@ scriptInjection("https://code.jquery.com/jquery-3.2.1.min.js", function () {
         var selectorTimer4 = $jq321("form[action='/cart/add']:first");
         var selectorTimer5 = $jq321("#shopify-section-product-template").find("form[action='/cart/add']").find("button[type='submit'],input[type='submit']").parent();
         var selectorTimer6 = $jq321("#shopify-section-product-template").find("form[action='/cart/add']");
-
+        
+        if (Shopify.shop == "6bccd5.myshopify.com") {
+                customSelector = $jq321("._rsi-buy-now-button");
+                finalSelector = customSelector[0];
+        }
 
     if (Shopify.shop == "f8c697.myshopify.com") {
         if(window.location.href == "https://spankygarbage.store/products/factory-price-flame-humidifier-aroma-diffusers-machine-home-bedroom-silent-essential-oil-flame-aroma-diffuser")
