3723a895a9342402765754d19f3329b60bb68cde
Roha Ali <123722826+rohaali12@users.noreply.github.com>
2024-01-15 12:20:20 +0500

    CTI-3012
    https://pms.vaivaltech.com/browse/CTI-3012

--- visitor/custom-location-visitor.js
+++ visitor/custom-location-visitor.js
@@ -3,7 +3,7 @@
  * @author CareCart
  * @link https://apps.shopify.com/partners/care-cart
  * @link https://carecart.io/
- * @version 6.05
+ * @version 6.13
  *
  * Any unauthorized use and distribution of this and related files, is strictly forbidden.
  * In case of any inquiries, please contact here: https://carecart.io/contact-us/
@@ -799,6 +799,9 @@ scriptInjection("https://code.jquery.com/jquery-3.2.1.min.js", function () {
     if (Shopify.shop == "0769d2.myshopify.com") {
         var meta = { "product": { "id": __st.rid } };
     }
+    else{
+        var meta = { "product": { "id": __st.rid } };
+    }
     
     $jq321.ajax({
         type: "GET",
@@ -829,6 +832,9 @@ scriptInjection("https://code.jquery.com/jquery-3.2.1.min.js", function () {
     let customSelctor = "";
     let finalSelector = "";
 
+     if (Shopify.shop == "simplemerit-store.myshopify.com") {
+        $jq321("head").append('<style type="text/css">.visitor-counter-content-box-carecartbysalespop-2020 { height: 50px !important;  margin-top: 0px !important;}</style>');
+     }
      if (Shopify.shop == "0769d2.myshopify.com") {
         customSelctor = $jq321(".product-single__add-to-cart");
         finalSelector = customSelctor[0];
