0471514665f183b0a16059df319550a6a5ae80b2
Roha Ali <123722826+rohaali12@users.noreply.github.com>
2024-01-25 15:18:16 +0500

    CTI-3071
    https://pms.vaivaltech.com/browse/CTI-3071

--- sales-pop/customization-salesnotifier.js
+++ sales-pop/customization-salesnotifier.js
@@ -3,7 +3,7 @@
  * @author CareCart
  * @link https://apps.shopify.com/partners/care-cart
  * @link https://carecart.io/
- * @version 6.31
+ * @version 6.37
  *
  * Any unauthorized use and distribution of this and related files, is strictly forbidden.
  * In case of any inquiries, please contact here: https://carecart.io/contact-us/
@@ -3854,6 +3854,11 @@ scriptInjection("https://code.jquery.com/jquery-3.2.1.min.js", function () {
         var selectorVisitor3 = $jq321("form[action='/cart/add']:first").find("button[type='submit'],input[type='submit']").parent();
         var selectorVisitor4 = $jq321("form[action='/cart/add']:first");
 
+	if (Shopify.shop == "a0bb54-3.myshopify.com") {
+            $jq321("head").append(
+                "<style type='text/css'>.visitor-counter-content-box-carecartbysalespop-2020 {margin-top: -30px !important;}</style>"
+            );
+        }
         if (Shopify.shop == "jetty-bra.myshopify.com") {
             masterSelector = $jq321(".quantity-selector");
             finalSelector = masterSelector[0];
