193e8ddc13b543c220a2c957785b2a8796404e72
Roha Ali <123722826+rohaali12@users.noreply.github.com>
2024-01-26 15:09:00 +0500

    CTI-3078
    https://pms.vaivaltech.com/browse/CTI-3078

--- stock/custom-location-stock.js
+++ stock/custom-location-stock.js
@@ -3,7 +3,7 @@
  * @author CareCart
  * @link https://apps.shopify.com/partners/care-cart
  * @link https://carecart.io/
- * @version 6.39
+ * @version 6.40
  *
  * Any unauthorized use and distribution of this and related files, is strictly forbidden.
  * In case of any inquiries, please contact here: https://carecart.io/contact-us/
@@ -1739,6 +1739,11 @@ scriptInjection("https://code.jquery.com/jquery-3.2.1.min.js", function () {
                 */
 
                 let firstSelector;
+
+                if (Shopify.shop == "decordsstore.myshopify.com") {
+                    $jq321("head").append('<style type="text/css">.section--template{margin-top:10px;} .cart-countdown-desktop-top-center{margin-bottom:28px;}</style>');
+                    firstSelector = $jq321(".shopify-section.cc-main-cart.section.section--template");
+                } 
                  if (Shopify.shop == "pura-health11.myshopify.com") {
                     firstSelector = $jq321("#CartDrawer-Form");
                 }
