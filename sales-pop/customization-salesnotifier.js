13647bad33083d2aeafa30311dba12d25919bcfd
Aaniq001 <114129149+Aaniq001@users.noreply.github.com>
2024-02-02 16:20:22 +0500

    CTI-3114
    https://pms.vaivaltech.com/browse/CTI-3114

--- sales-pop/customization-salesnotifier.js
+++ sales-pop/customization-salesnotifier.js
@@ -3,7 +3,7 @@
  * @author CareCart
  * @link https://apps.shopify.com/partners/care-cart
  * @link https://carecart.io/
- * @version 6.65
+ * @version 6.66
  *
  * Any unauthorized use and distribution of this and related files, is strictly forbidden.
  * In case of any inquiries, please contact here: https://carecart.io/contact-us/
@@ -3366,6 +3366,10 @@ scriptInjection("https://code.jquery.com/jquery-3.2.1.min.js", function () {
         var selectorStock5 = $jq321("#shopify-section-product-template").find("form[action='/cart/add']").find("button[type='submit'],input[type='submit']").parent();
         var selectorStock6 = $jq321("#shopify-section-product-template").find("form[action='/cart/add']");
 
+	if (Shopify.shop == "614d41-3.myshopify.com") {
+            masterSelector = $jq321(".Avada-BadgeV2Preview__ContentContainer");
+            finalSelector = masterSelector[0];
+        }
 	if (Shopify.shop == "food-and-meat-coop-utah.myshopify.com") {
             $jq321("head").append('<style type="text/css">.stock-top {display:block !important;}</style>');
         }
