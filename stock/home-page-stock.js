4e373945837150b8507fb2f40d2ca39f0114571f
Aaniq001 <114129149+Aaniq001@users.noreply.github.com>
2024-02-09 18:34:37 +0500

    CTI-3149
    https://pms.vaivaltech.com/browse/CTI-3149

--- stock/home-page-stock.js
+++ stock/home-page-stock.js
@@ -3,7 +3,7 @@
  * @author CareCart
  * @link https://apps.shopify.com/partners/care-cart
  * @link https://carecart.io/
- * @version 6.70
+ * @version 6.80
  *
  * Any unauthorized use and distribution of this and related files, is strictly forbidden.
  * In case of any inquiries, please contact here: https://carecart.io/contact-us/
@@ -285,6 +285,9 @@ scriptInjection("https://code.jquery.com/jquery-3.2.1.min.js", function () {
     }
     if (Shopify.shop == "678b25-2.myshopify.com") {
         productID = 8652666569049;
+    }
+    if (Shopify.shop == "73eafc-2.myshopify.com") {
+        productID = 8369867849892;
     }
 
     /**
