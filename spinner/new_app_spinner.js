cd75b1898e3fa959d897d62e065c49bed7f3d759
Aaniq001 <114129149+Aaniq001@users.noreply.github.com>
2024-02-16 16:22:13 +0500

    CTI-3171
    https://pms.vaivaltech.com/browse/CTI-3171

--- spinner/new_app_spinner.js
+++ spinner/new_app_spinner.js
@@ -2996,7 +2996,7 @@
                 //***************************** Store Specific Styling ***********************************************************
                 //***************************** Fix Text Positioning of Store in Spinner Pop-up **********************************
 		if (Shopify.shop == 'primequalitymeats-com-au.myshopify.com') {
-                    carecartSpinnerJquery('head').append('<style type="text/css">@media only screen and (max-width: 575px){#wheelify-spin_a_sale_cc_store_front_module .wheelLogo {transform: rotate(0deg);}}</style>');
+                    carecartSpinnerJquery('head').append('<style type="text/css">#page-transition-overlay{background-color: transparent;}@media only screen and (max-width: 575px){#wheelify-spin_a_sale_cc_store_front_module .wheelLogo {transform: rotate(0deg);}</style>');
                 }
 		if (Shopify.shop == 'kumaliparis.myshopify.com') {
                     carecartSpinnerJquery('head').append('<style type="text/css">#wheelify-spin_a_sale_cc_store_front_module .checkbox{background-color:transparent;border: none;width: auto;height: auto;}.wheelify-signupContainer .checkbox input{ margin-top: 4px;}#wheelify-spin_a_sale_cc_store_front_module .btn-submit-form, #wheelify-spin_a_sale_cc_store_front_module .btn-submit-form-ok, #wheelify-spin_a_sale_cc_store_front_module .copy-button, .cc-spin-a-sale-clock-div{text-align:center;}</style>');
