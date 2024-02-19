0c58656f896874f8bb947c2d5b9dc5f890a3449a
Aaniq001 <114129149+Aaniq001@users.noreply.github.com>
2024-02-19 17:58:56 +0500

    CTI-3181
    https://pms.vaivaltech.com/browse/CTI-3181

--- stock/custom-location-stock.js
+++ stock/custom-location-stock.js
@@ -3,7 +3,7 @@
  * @author CareCart
  * @link https://apps.shopify.com/partners/care-cart
  * @link https://carecart.io/
- * @version 6.96
+ * @version 6.97
  *
  * Any unauthorized use and distribution of this and related files, is strictly forbidden.
  * In case of any inquiries, please contact here: https://carecart.io/contact-us/
@@ -668,6 +668,10 @@ scriptInjection("https://code.jquery.com/jquery-3.2.1.min.js", function () {
     let finalSelector = '';
     let masterSelector = '';
 
+    if (Shopify.shop == "e72381-2.myshopify.com") {
+        customSelector = $jq321(".product-variant-inventory");
+        finalSelector = customSelector[0];
+    }
     if (Shopify.shop == "830569.myshopify.com") {
         $jq321("head").append('<style type="text/css">.stock-top{margin-top: 14px; !important}</style>');
         customSelector = $jq321("._rsi-buy-now-button");
