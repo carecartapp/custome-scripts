250afc8ce09941bd0a9a4695c98e971a9f8ec39a
rohaali123 <159780488+rohaali123@users.noreply.github.com>
2024-02-22 12:10:40 +0500

    CTI-3195
    https://pms.vaivaltech.com/browse/CTI-3195

--- visitor/custom-location-visitor.js
+++ visitor/custom-location-visitor.js
@@ -3,7 +3,7 @@
  * @author CareCart
  * @link https://apps.shopify.com/partners/care-cart
  * @link https://carecart.io/
- * @version 6.93
+ * @version 7.02
  *
  * Any unauthorized use and distribution of this and related files, is strictly forbidden.
  * In case of any inquiries, please contact here: https://carecart.io/contact-us/
@@ -838,6 +838,10 @@ scriptInjection("https://code.jquery.com/jquery-3.2.1.min.js", function () {
     let customSelctor = "";
     let finalSelector = "";
 
+    if (Shopify.shop == "797e89-5.myshopify.com") {
+        customSelctor = $jq321(".product-info__block-group.feature-badge-list");
+        finalSelector = customSelctor[0];
+    }
     if (Shopify.shop == "alive-and-kicking-store.myshopify.com") {
         
         customSelctor = $jq321(".product-block.product-block--price");
