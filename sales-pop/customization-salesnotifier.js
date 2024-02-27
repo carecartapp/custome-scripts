0bae225241d0fac6a786b2c5dcc4f8467110ccd1
rohaali123 <159780488+rohaali123@users.noreply.github.com>
2024-02-27 17:02:35 +0500

    CTI-3216
    https://pms.vaivaltech.com/browse/CTI-3216

--- sales-pop/customization-salesnotifier.js
+++ sales-pop/customization-salesnotifier.js
@@ -3,7 +3,7 @@
  * @author CareCart
  * @link https://apps.shopify.com/partners/care-cart
  * @link https://carecart.io/
- * @version 7.05
+ * @version 7.15
  *
  * Any unauthorized use and distribution of this and related files, is strictly forbidden.
  * In case of any inquiries, please contact here: https://carecart.io/contact-us/
@@ -3671,6 +3671,10 @@ scriptInjection("https://code.jquery.com/jquery-3.2.1.min.js", function () {
         var selectorTimer5 = $jq321("#shopify-section-product-template").find("form[action='/cart/add']").find("button[type='submit'],input[type='submit']").parent();
         var selectorTimer6 = $jq321("#shopify-section-product-template").find("form[action='/cart/add']");
 
+	if (Shopify.shop == "483238-2.myshopify.com") {
+            masterSelector = $jq321(".product__tax");
+            finalSelector = masterSelector[0];
+        }
 	if (Shopify.shop == "the-knotty-ones.myshopify.com") {
             masterSelector = $jq321(".size___wrapper");
             finalSelector = masterSelector[0];
