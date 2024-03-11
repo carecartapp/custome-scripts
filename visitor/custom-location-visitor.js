f93f3480bff435ae9ece94b2727a35b6f5d30b05
rohaali123 <159780488+rohaali123@users.noreply.github.com>
2024-03-11 12:36:32 +0500

    CTI-3270
    CTI-3270

--- visitor/custom-location-visitor.js
+++ visitor/custom-location-visitor.js
@@ -3,7 +3,7 @@
  * @author CareCart
  * @link https://apps.shopify.com/partners/care-cart
  * @link https://carecart.io/
- * @version 7.35
+ * @version 7.37
  *
  * Any unauthorized use and distribution of this and related files, is strictly forbidden.
  * In case of any inquiries, please contact here: https://carecart.io/contact-us/
@@ -839,7 +839,7 @@ scriptInjection("https://code.jquery.com/jquery-3.2.1.min.js", function () {
     let finalSelector = "";
     
     if (Shopify.shop == "8a2893.myshopify.com") {
-            customSelctor = $jq321(".product__description");
+        customSelctor = $jq321(".product-form__submit");
         finalSelector = customSelctor[0];
     }
     if (Shopify.shop == "myexportleftovers.myshopify.com") {
