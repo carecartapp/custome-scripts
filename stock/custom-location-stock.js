9f1a72a20904adb5ef6f51737498a46233311741
rohaali123 <159780488+rohaali123@users.noreply.github.com>
2024-04-01 14:37:54 +0500

    CTI-3356
    CTI-3356

--- stock/custom-location-stock.js
+++ stock/custom-location-stock.js
@@ -3,7 +3,7 @@
  * @author CareCart
  * @link https://apps.shopify.com/partners/care-cart
  * @link https://carecart.io/
- * @version 7.71
+ * @version 7.74
  *
  * Any unauthorized use and distribution of this and related files, is strictly forbidden.
  * In case of any inquiries, please contact here: https://carecart.io/contact-us/
@@ -668,6 +668,10 @@ scriptInjection("https://code.jquery.com/jquery-3.2.1.min.js", function () {
     let finalSelector = '';
     let masterSelector = '';
 
+    if (Shopify.shop == "50b650-21.myshopify.com") {
+        customSelector = $jq321(".product-info__liquid");
+        finalSelector = customSelector[0];
+    }
     if (Shopify.shop == "f985c7-2.myshopify.com") {
         customSelector = $jq321(".product__submit__buttons");
         finalSelector = customSelector[0];
