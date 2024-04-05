e5c95a41507c94eccaa23cdbc0f8a71e01ef6c82
rohaali123 <159780488+rohaali123@users.noreply.github.com>
2024-04-05 14:09:38 +0500

    CTI-3372
    CTI-3372

--- sales-pop/customization-salesnotifier.js
+++ sales-pop/customization-salesnotifier.js
@@ -3,7 +3,7 @@
  * @author CareCart
  * @link https://apps.shopify.com/partners/care-cart
  * @link https://carecart.io/
- * @version 7.76
+ * @version 7.79
  * Any unauthorized use and distribution of this and related files, is strictly forbidden.
  * In case of any inquiries, please contact here: https://carecart.io/contact-us/
  */
@@ -3697,6 +3697,10 @@ scriptInjection("https://code.jquery.com/jquery-3.2.1.min.js", function () {
         var selectorTimer5 = $jq321("#shopify-section-product-template").find("form[action='/cart/add']").find("button[type='submit'],input[type='submit']").parent();
         var selectorTimer6 = $jq321("#shopify-section-product-template").find("form[action='/cart/add']");
 
+	if (Shopify.shop == "myfeedneed.myshopify.com") {
+            masterSelector = $jq321(".new-form-atc.addCart");
+            finalSelector = masterSelector[0];
+        }
 	if (Shopify.shop == "483238-2.myshopify.com") {
             masterSelector = $jq321(".form__label");
             finalSelector = masterSelector[0];
