dc9c09e8868ec9bb1b56ef0381f3dcf4a6cbf49b
Aaniq001 <114129149+Aaniq001@users.noreply.github.com>
2024-04-25 17:23:52 +0500

    CTI-3434/3436
    Text of stock bar is not showing up/Countdown timer and stock bar are broken/cart countdown timer are not showing up

--- stock/custom-location-stock.js
+++ stock/custom-location-stock.js
@@ -3,7 +3,7 @@
  * @author CareCart
  * @link https://apps.shopify.com/partners/care-cart
  * @link https://carecart.io/
- * @version 8.07
+ * @version 8.06
  *
  * Any unauthorized use and distribution of this and related files, is strictly forbidden.
  * In case of any inquiries, please contact here: https://carecart.io/contact-us/
