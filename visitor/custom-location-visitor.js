2cfa4284b2a66a8ab3499a419017ed040ddba5d1
Aaniq001 <114129149+Aaniq001@users.noreply.github.com>
2024-04-30 16:35:26 +0500

    CTI-3448
    Add Visitor Bar at mentioned place

--- visitor/custom-location-visitor.js
+++ visitor/custom-location-visitor.js
@@ -3,7 +3,7 @@
  * @author CareCart
  * @link https://apps.shopify.com/partners/care-cart
  * @link https://carecart.io/
- * @version 8.11
+ * @version 8.13
  *
  * Any unauthorized use and distribution of this and related files, is strictly forbidden.
  * In case of any inquiries, please contact here: https://carecart.io/contact-us/
@@ -1558,8 +1558,8 @@ scriptInjection("https://code.jquery.com/jquery-3.2.1.min.js", function () {
         finalSelector = customSelctor[0];
     }
     if (Shopify.shop == "saintmuze2.myshopify.com") {
-        $jq321("head").append('<style type="text/css">' + '.visitor-counter-content-box-carecartbysalespop-2020{margin-top: -45px !important; margin-left: 42px !important;}' + '</style>');
-        customSelctor = $jq321(".ga9P1_QRYm");
+        $jq321("head").append('<style type="text/css">' + '.visitor-counter-content-box-carecartbysalespop-2020{margin-top: -60px !important; margin-left: 0px !important;}' + '</style>');
+        customSelctor = $jq321(".gBaOaMs3o-");
         finalSelector = customSelctor[0];
     }
     if (Shopify.shop == "aee2a8.myshopify.com") {
