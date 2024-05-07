b9b29a95dc16ff95525cf34529a3127e4f91e177
rohaali123 <159780488+rohaali123@users.noreply.github.com>
2024-05-07 16:11:47 +0500

    CTI-3480
    CTI-3480

--- visitor/custom-location-visitor.js
+++ visitor/custom-location-visitor.js
@@ -3,7 +3,7 @@
  * @author CareCart
  * @link https://apps.shopify.com/partners/care-cart
  * @link https://carecart.io/
- * @version 8.16
+ * @version 8.17
  *
  * Any unauthorized use and distribution of this and related files, is strictly forbidden.
  * In case of any inquiries, please contact here: https://carecart.io/contact-us/
@@ -838,6 +838,11 @@ scriptInjection("https://code.jquery.com/jquery-3.2.1.min.js", function () {
     let customSelctor = "";
     let finalSelector = "";
 
+    if (Shopify.shop == "a6cefd-9b.myshopify.com") {
+        customSelctor = $jq321(".addly_widget_interact_qbreaks");
+        finalSelector = customSelctor[0];
+        $jq321("head").append('<style type="text/css">.visitor-counter-content-box-carecartbysalespop-2020 { height : 40px !important; margin-top: -10px !important;}</style>');
+    }
     if (Shopify.shop == "d3cbe5-4.myshopify.com") {
         customSelctor = $jq321(".pdp-action-container");
         finalSelector = customSelctor[0];
