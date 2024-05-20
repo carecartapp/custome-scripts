6d07c49f2417c0dce1fb05dffd988ad8f8b53e3e
rohaali123 <159780488+rohaali123@users.noreply.github.com>
2024-05-20 16:26:17 +0500

    CTI-3524
    CTI-3524

--- visitor/custom-location-visitor.js
+++ visitor/custom-location-visitor.js
@@ -3,7 +3,7 @@
  * @author CareCart
  * @link https://apps.shopify.com/partners/care-cart
  * @link https://carecart.io/
- * @version 8.23
+ * @version 8.31
  *
  * Any unauthorized use and distribution of this and related files, is strictly forbidden.
  * In case of any inquiries, please contact here: https://carecart.io/contact-us/
@@ -838,6 +838,11 @@ scriptInjection("https://code.jquery.com/jquery-3.2.1.min.js", function () {
     let customSelctor = "";
     let finalSelector = "";
 
+    if (Shopify.shop == "1e9ea6-c6.myshopify.com") {
+        customSelctor = $jq321("button.sc-ksHpcM");
+        finalSelector = customSelctor[0];
+        $jq321("head").append('<style type="text/css">.visitor-counter-content-box-carecartbysalespop-2020 {height: 35px !important;}</style>');
+    }
     if (Shopify.shop == "waterloo-bathrooms-store.myshopify.com") {
         customSelctor = $jq321(".product-form--atc");
         $jq321("head").append('<style type="text/css">.visitor-counter-content-box-carecartbysalespop-2020 { height : 35px !important; margin-top: 0px !important;}</style>');        
