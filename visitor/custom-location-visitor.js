4fee7d7cb62295fb519fac2cfed0595e790f87ac
Aaniq001 <114129149+Aaniq001@users.noreply.github.com>
2024-06-20 15:26:26 +0500

    CTI-3615
    Title: Place the Visitor bar on homepage

--- visitor/custom-location-visitor.js
+++ visitor/custom-location-visitor.js
@@ -3,7 +3,7 @@
  * @author CareCart
  * @link https://apps.shopify.com/partners/care-cart
  * @link https://carecart.io/
- * @version 8.58
+ * @version 8.68
  *
  * Any unauthorized use and distribution of this and related files, is strictly forbidden.
  * In case of any inquiries, please contact here: https://carecart.io/contact-us/
@@ -844,6 +844,10 @@ scriptInjection("https://code.jquery.com/jquery-3.2.1.min.js", function () {
     let customSelctor = "";
     let finalSelector = "";
 
+    if (Shopify.shop == "49279d-2.myshopify.com") {
+        customSelctor = $jq321(".product-form__submit");
+        finalSelector = customSelctor[0];
+    }
     if (Shopify.shop == "b5e750-10.myshopify.com") {
         customSelctor = $jq321(".shopify-product-form");
         finalSelector = customSelctor[0];
