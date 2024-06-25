518965251be220b1186806f94f65280701fadd62
Aaniq001 <114129149+Aaniq001@users.noreply.github.com>
2024-06-25 15:48:25 +0500

    CTI-3667
    title: Visitor counter Disappears after the page is translated to English.

--- visitor/custom-location-visitor.js
+++ visitor/custom-location-visitor.js
@@ -3,7 +3,7 @@
  * @author CareCart
  * @link https://apps.shopify.com/partners/care-cart
  * @link https://carecart.io/
- * @version 8.71
+ * @version 8.76
  *
  * Any unauthorized use and distribution of this and related files, is strictly forbidden.
  * In case of any inquiries, please contact here: https://carecart.io/contact-us/
@@ -811,6 +811,9 @@ scriptInjection("https://code.jquery.com/jquery-3.2.1.min.js", function () {
     if (Shopify.shop == "b5e750-10.myshopify.com") {
         var meta = { "product": { "id": __st.rid } };
     }
+    if (Shopify.shop == "55685d-e7.myshopify.com") {
+        var meta = { "product": { "id": __st.rid } };
+    }
     else{
         var meta = { "product": { "id": __st.rid } };
     }
@@ -844,6 +847,10 @@ scriptInjection("https://code.jquery.com/jquery-3.2.1.min.js", function () {
     let customSelctor = "";
     let finalSelector = "";
 
+    if (Shopify.shop == "55685d-e7.myshopify.com") {
+        customSelctor = $jq321(".product-form__buttons");
+        finalSelector = customSelctor[0];
+    }
     if (Shopify.shop == "49279d-2.myshopify.com") {
         customSelctor = $jq321(".product-form__submit");
         finalSelector = customSelctor[0];
