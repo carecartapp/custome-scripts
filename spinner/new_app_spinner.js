37b98bf08e7cd8a33ca7c2a7c207114e06a37eaf
Aaniq001 <114129149+Aaniq001@users.noreply.github.com>
2024-06-27 19:02:59 +0500

    CTI-3678
    Title: Wheel Preview & Live Not Working | Copy Code Button Color Check Required

--- spinner/new_app_spinner.js
+++ spinner/new_app_spinner.js
@@ -1,5 +1,5 @@
 //******* @author: CareCart App-Wheelify*******************************************
-//****** Store Frontend JS - carecartSpinnerApp.js GH v.6.0.0 - Build ver 8.80 *******************
+//****** Store Frontend JS - carecartSpinnerApp.js GH v.6.0.0 - Build ver 8.81 *******************
 //****** Updated at: 12-June-2024, 04:35 PM  ********************************************************
 
 (function () {
@@ -3035,8 +3035,8 @@
                 //***************************** Fix Text Positioning of Store in Spinner Pop-up **********************************
 
 		if (Shopify.shop == 'u-tec.myshopify.com') {
-                    carecartSpinnerJquery('body').append("<style type='text/css'>#wheelify-spin_a_sale_cc_store_front_module .btn-submit-form, #wheelify-spin_a_sale_cc_store_front_module .btn-submit-form-ok, #wheelify-spin_a_sale_cc_store_front_module .copy-button { color: #384f66 !important; }</style>");
-                }
+                    carecartSpinnerJquery('body').append("<style type='text/css'>#wheelify-spin_a_sale_cc_store_front_module .btn-submit-form, #wheelify-spin_a_sale_cc_store_front_module .btn-submit-form-ok, #wheelify-spin_a_sale_cc_store_front_module .copy-button { color: #384f66 !important; } #wheelify-spin_a_sale_cc_store_front_module .btn-submit-form .fa , #wheelify-spin_a_sale_cc_store_front_module .btn-submit-form-ok .fa , #wheelify-spin_a_sale_cc_store_front_module .copy-button .fa { color: #384f66 !important; padding-right: 4px !important; padding-bottom: 3px !important;</style>");
+            	}
 		if (Shopify.shop == 'lefeetscooter.myshopify.com') {
 		    carecartSpinnerJquery('body').append("<style type='text/css'>#wheelify-spin_a_sale_cc_store_front_module .checkbox { width: auto; margin-bottom: 40px; border: none; background: transparent; box-shadow: none;} #wheelify-spin_a_sale_cc_store_front_module .btn-submit-form, #wheelify-spin_a_sale_cc_store_front_module .btn-submit-form-ok, #wheelify-spin_a_sale_cc_store_front_module .copy-button, .cc-spin-a-sale-clock-div {text-align: center;}</style>");
 	   	}
