1570370d261d4d21aebe1131bf0f7d90f74c4d53
YasirVaival <145327125+YasirVaival@users.noreply.github.com>
2024-07-01 16:57:46 +0500

    CTI-3689
    Title: Visitor counter not appearing

--- visitor/custom-location-visitor.js
+++ visitor/custom-location-visitor.js
@@ -3,7 +3,7 @@
  * @author CareCart
  * @link https://apps.shopify.com/partners/care-cart
  * @link https://carecart.io/
- * @version 8.85
+ * @version 8.90
  *
  * Any unauthorized use and distribution of this and related files, is strictly forbidden.
  * In case of any inquiries, please contact here: https://carecart.io/contact-us/
@@ -847,6 +847,12 @@ scriptInjection("https://code.jquery.com/jquery-3.2.1.min.js", function () {
     let customSelctor = "";
     let finalSelector = "";
 
+    if (Shopify.shop == "shinplex.myshopify.com") {
+        customSelctor = $jq321(".rio-options");
+        finalSelector = customSelctor[0];
+        $jq321("head").append('<style type="text/css">.visitor-counter-content-box-carecartbysalespop-2020 {margin-left: -130px ;}</style>');
+    }
+    
     if (Shopify.shop == "6a0f21-2.myshopify.com") {
         customSelctor = $jq321(".product_custom_liquid");
         finalSelector = customSelctor[0];
