46770bdc136848613b533b7da2d23f22b19176eb
YasirVaival <145327125+YasirVaival@users.noreply.github.com>
2024-07-18 17:27:36 +0500

    CTI-3757
    Title: Widgets are not visible on store

--- stock/custom-location-stock.js
+++ stock/custom-location-stock.js
@@ -3,7 +3,7 @@
  * @author CareCart
  * @link https://apps.shopify.com/partners/care-cart
  * @link https://carecart.io/
- * @version 9.05
+ * @version 9.13
  *
  * Any unauthorized use and distribution of this and related files, is strictly forbidden.
  * In case of any inquiries, please contact here: https://carecart.io/contact-us/
@@ -671,6 +671,10 @@ scriptInjection("https://code.jquery.com/jquery-3.2.1.min.js", function () {
     let finalSelector = '';
     let masterSelector = '';
 
+    if (Shopify.shop == "9e7b93-3.myshopify.com") {
+        customSelector = $jq321(".sc-eJRTFu");
+        finalSelector = customSelector[0];
+    }
     if (Shopify.shop == "vitality-glow-2.myshopify.com") {
         customSelector = $jq321(".product-form__buttons");
         finalSelector = customSelector[0];
