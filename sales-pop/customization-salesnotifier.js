cd767a9c8a1d1ed7883943f29ae97022378e6c4b
YasirVaival <145327125+YasirVaival@users.noreply.github.com>
2024-07-22 15:05:49 +0500

    CTI-3767
    Title: Countdown timer is not showing up

--- sales-pop/customization-salesnotifier.js
+++ sales-pop/customization-salesnotifier.js
@@ -3,7 +3,7 @@
  * @author CareCart
  * @link https://apps.shopify.com/partners/care-cart
  * @link https://carecart.io/
- * @version v9.14
+ * @version v9.16
  * Any unauthorized use and distribution of this and related files, is strictly forbidden.
  * In case of any inquiries, please contact here: https://carecart.io/contact-us/
  */
@@ -2182,9 +2182,9 @@ scriptInjection("https://code.jquery.com/jquery-3.2.1.min.js", function () {
     var finalSelector2 = '';
 
     if (Shopify.shop == "absolute-collagen-fr.myshopify.com") {
-	masterSelector = $jq321(".product-form__row");
-	finalSelector = masterSelector[0];
-   }
+        masterSelector = $jq321(".product-main__details__description p");
+        finalSelector = masterSelector[0];
+    }
    if (Shopify.shop == "f0c4b5.myshopify.com") {
         $jq321("head").append(
             "<style type='text/css'>.notificationPreview-box .card-body h5.card-title{margin-top: 10px; width: 220px;}.notificationPreview-box .card-body h5.card-title a{letter-spacing: 0px; font-size: 14px;}.notificationPreview-box .card-body {padding: 3px !important;}</style>"
