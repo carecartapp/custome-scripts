0c4dc0ea88d0c1798a7c6495a32982e7106fc6ca
YasirVaival <145327125+YasirVaival@users.noreply.github.com>
2024-07-31 15:38:28 +0500

    CTI-3796
    Place the Visitor bar above to the add to cart button

--- visitor/custom-location-visitor.js
+++ visitor/custom-location-visitor.js
@@ -3,7 +3,7 @@
  * @author CareCart
  * @link https://apps.shopify.com/partners/care-cart
  * @link https://carecart.io/
- * @version 9.26
+ * @version 9.27
  *
  * Any unauthorized use and distribution of this and related files, is strictly forbidden.
  * In case of any inquiries, please contact here: https://carecart.io/contact-us/
@@ -853,6 +853,10 @@ scriptInjection("https://code.jquery.com/jquery-3.2.1.min.js", function () {
     let customSelctor = "";
     let finalSelector = "";
 
+    if (Shopify.shop == "6a8848-dd.myshopify.com") {
+        customSelctor = $jq321(".ProductForm__AddToCart");
+        finalSelector = customSelctor[0];
+    }
     if (Shopify.shop == "664a58-ca.myshopify.com") {
         customSelctor = $jq321(".product-buy-buttons--primary");
         finalSelector = customSelctor[0];
