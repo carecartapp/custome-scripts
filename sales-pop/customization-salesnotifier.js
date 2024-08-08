dff135ceb39065a7fd40bb357cc0476e4499c92d
YasirVaival <145327125+YasirVaival@users.noreply.github.com>
2024-08-08 13:35:57 +0500

    CTI-3840
    Ticket: CTI-3840
    Title: Widgets Format Fix Required

--- sales-pop/customization-salesnotifier.js
+++ sales-pop/customization-salesnotifier.js
@@ -3,7 +3,7 @@
  * @author CareCart
  * @link https://apps.shopify.com/partners/care-cart
  * @link https://carecart.io/
- * @version v9.40
+ * @version v9.43
  * Any unauthorized use and distribution of this and related files, is strictly forbidden.
  * In case of any inquiries, please contact here: https://carecart.io/contact-us/
  */
@@ -3848,6 +3848,10 @@ scriptInjection("https://code.jquery.com/jquery-3.2.1.min.js", function () {
         var selectorTimer5 = $jq321("#shopify-section-product-template").find("form[action='/cart/add']").find("button[type='submit'],input[type='submit']").parent();
         var selectorTimer6 = $jq321("#shopify-section-product-template").find("form[action='/cart/add']");
 
+	if (Shopify.shop == "arainbull.myshopify.com") {
+            masterSelector = $jq321(".t4s-product-form__buttons");
+            finalSelector = masterSelector[0];
+        }
 	if (Shopify.shop == "marjjan.myshopify.com") {
             masterSelector = $jq321(".t4s-product-form__buttons");
             finalSelector = masterSelector[0];
@@ -5045,7 +5049,11 @@ scriptInjection("https://code.jquery.com/jquery-3.2.1.min.js", function () {
             else if (Shopify.shop == "d67b73-2.myshopify.com") {
                 $jq321("head").append('<style type="text/css">.shopify-product-form .BadgesImgBox label{visibility: visible !important;}#CloneBox{margin-top: -80px;}</style>');
                 $jq321(trustBadgesResponse.view).insertAfter('.form-inpage');
-            }    
+            } else if (Shopify.shop == "arainbull.myshopify.com") {
+                masterSelectortr = $jq321(".shopify-payment-button");
+                finalSelectortr = masterSelectortr[0];
+                $jq321(trustBadgesResponse.view).insertAfter(finalSelectortr);
+            }
             else if (Shopify.shop == "c59bad-2.myshopify.com") {
                 $jq321(trustBadgesResponse.view).insertAfter('#m-1685977443552');
             }
