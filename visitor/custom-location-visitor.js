ad0cdc2de5aad073126a84b3e75aae71bae1b320
YasirVaival <145327125+YasirVaival@users.noreply.github.com>
2024-08-08 13:31:47 +0500

    CTI-3842
    Traffic Counter is not appearing on Product Pages

--- visitor/custom-location-visitor.js
+++ visitor/custom-location-visitor.js
@@ -3,7 +3,7 @@
  * @author CareCart
  * @link https://apps.shopify.com/partners/care-cart
  * @link https://carecart.io/
- * @version 9.29
+ * @version 9.42
  *
  * Any unauthorized use and distribution of this and related files, is strictly forbidden.
  * In case of any inquiries, please contact here: https://carecart.io/contact-us/
@@ -853,6 +853,10 @@ scriptInjection("https://code.jquery.com/jquery-3.2.1.min.js", function () {
     let customSelctor = "";
     let finalSelector = "";
 
+    if (Shopify.shop == "zobhalife.myshopify.com") {
+        customSelctor = $jq321(".productView-group");
+        finalSelector = customSelctor[0];
+    }
     if (Shopify.shop == "26311d-3.myshopify.com") {
         $jq321("head").append('<style type="text/css">.visitor-counter-content-box-carecartbysalespop-2020 {margin: -5px ; white-space: nowrap; }</style>');
     }
