5736e57ab886f156e267c9c998232a97a817f87a
YasirVaival <145327125+YasirVaival@users.noreply.github.com>
2024-08-28 17:52:54 +0500

    CTI-3910
    Changing font colors to black

--- spinner/new_app_spinner.js
+++ spinner/new_app_spinner.js
@@ -1,5 +1,5 @@
 //******* @author: CareCart App-Wheelify*******************************************
-//****** Store Frontend JS - carecartSpinnerApp.js GH v.6.0.0 - Build ver 9.68 *******************
+//****** Store Frontend JS - carecartSpinnerApp.js GH v.6.0.0 - Build ver 9.70 *******************
 //****** Updated at: 12-June-2024, 04:35 PM  ********************************************************
 
 (function () {
@@ -3045,7 +3045,10 @@
                 //***************** End - Countdown Timer function min & sec ********************
                 //***************************** Store Specific Styling ***********************************************************
                 //***************************** Fix Text Positioning of Store in Spinner Pop-up **********************************
-		
+
+		if (Shopify.shop == 'ngurrbulbaadhinclothing.myshopify.com') {
+                    carecartSpinnerJquery('body').append("<style type='text/css'> #wheelify-spin-trigger-cc span { color: #161616 !important; } .wheelify-text-heading {color: #161616 !important; } span#cc-spin-a-sale-consent-text { color: #161616 !important; } .wheelify-text-description { color: #161616 !important; }</style>");
+                }
 		if (Shopify.shop == 'tracyeinnysg.myshopify.com') {
 	            carecartSpinnerJquery('body').append("<style type='text/css'>.wheelify-signupContainer .checkbox label {margin-bottom: 12px;} #wheelify-spin_a_sale_cc_store_front_module .checkbox{display: contents;} .wheelify-signupContainer .checkbox input {margin-top: 4px;} #wheelify-spin_a_sale_cc_store_front_module .btn-submit-form, #wheelify-spin_a_sale_cc_store_front_module .btn-submit-form-ok, #wheelify-spin_a_sale_cc_store_front_module .copy-button {text-align: center;}#wheelify-spin_a_sale_cc_store_front_module .wheelify-ContentRight {margin-top: 50px !important;} </style>"); 
 	        }
