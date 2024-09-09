55de8d85dbb1a795553821802480ad7f686a7744
YasirVaival <145327125+YasirVaival@users.noreply.github.com>
2024-09-09 15:18:27 +0500

    CTI-3945
    Widget disappears upon changing language

--- stock/custom-location-stock.js
+++ stock/custom-location-stock.js
@@ -3,7 +3,7 @@
  * @author CareCart
  * @link https://apps.shopify.com/partners/care-cart
  * @link https://carecart.io/
- * @version 9.79
+ * @version 9.89
  *
  * Any unauthorized use and distribution of this and related files, is strictly forbidden.
  * In case of any inquiries, please contact here: https://carecart.io/contact-us/
@@ -671,6 +671,10 @@ scriptInjection("https://code.jquery.com/jquery-3.2.1.min.js", function () {
     let finalSelector = '';
     let masterSelector = '';
 
+    if (Shopify.shop == "c0ee03-09.myshopify.com") {
+        customSelector = $jq321("#es-form-button");
+        finalSelector = customSelector[0];
+    }
     if (Shopify.shop == "eb64a0-00.myshopify.com") {
         customSelector = $jq321(".pf-button-1");
         finalSelector = customSelector[0];
