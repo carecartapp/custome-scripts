35fbf9e316aff06f4398a0b1742b643e6b72ffd5
YasirVaival <145327125+YasirVaival@users.noreply.github.com>
2024-09-10 13:19:18 +0500

    CTI-3948
    Stock Countdown is not working

--- stock/custom-location-stock.js
+++ stock/custom-location-stock.js
@@ -3,7 +3,7 @@
  * @author CareCart
  * @link https://apps.shopify.com/partners/care-cart
  * @link https://carecart.io/
- * @version 9.89
+ * @version 9.91
  *
  * Any unauthorized use and distribution of this and related files, is strictly forbidden.
  * In case of any inquiries, please contact here: https://carecart.io/contact-us/
@@ -671,6 +671,10 @@ scriptInjection("https://code.jquery.com/jquery-3.2.1.min.js", function () {
     let finalSelector = '';
     let masterSelector = '';
 
+    if (Shopify.shop == "bb0bab-d2.myshopify.com") {
+        customSelector = $jq321(".product-form__payment-container");
+        finalSelector = customSelector[0];
+    }
     if (Shopify.shop == "c0ee03-09.myshopify.com") {
         customSelector = $jq321("#es-form-button");
         finalSelector = customSelector[0];
