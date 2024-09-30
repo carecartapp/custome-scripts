e67f31b4dc46123889766e74c1ff842b655ffd66
YasirVaival <145327125+YasirVaival@users.noreply.github.com>
2024-09-30 17:15:04 +0500

    CTI-4014
    Widget is not showing up on Product Pages

--- visitor/custom-location-visitor.js
+++ visitor/custom-location-visitor.js
@@ -3,7 +3,7 @@
  * @author CareCart
  * @link https://apps.shopify.com/partners/care-cart
  * @link https://carecart.io/
- * @version 10.17
+ * @version 10.22
  *
  * Any unauthorized use and distribution of this and related files, is strictly forbidden.
  * In case of any inquiries, please contact here: https://carecart.io/contact-us/
@@ -853,6 +853,11 @@ scriptInjection("https://code.jquery.com/jquery-3.2.1.min.js", function () {
     let customSelctor = "";
     let finalSelector = "";
 
+    if (Shopify.shop == "2b53d1-cf.myshopify.com") {
+        $jq321("head").append('<style type="text/css">.visitor-counter-content-box-carecartbysalespop-2020 {margin-bottom: -40px !important; margin-top: 0px !important;}</style>');
+        customSelctor = $jq321(".js.product-form__input.bottom-space.js-product-option-form");
+        finalSelector = customSelctor[0];
+    }
     if (Shopify.shop == "riccardo-garzoli.myshopify.com") {
         $jq321(function() {
             // Fix for translating widget store based
