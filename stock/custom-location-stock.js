8f8ee7a5e249476cf77eaf96ce9bb0cea6c38c2c
YasirVaival <145327125+YasirVaival@users.noreply.github.com>
2024-10-01 16:03:09 +0500

    CTI-4022
    Adjust Widgets Above ATC and Buy Now buttons.

--- stock/custom-location-stock.js
+++ stock/custom-location-stock.js
@@ -3,7 +3,7 @@
  * @author CareCart
  * @link https://apps.shopify.com/partners/care-cart
  * @link https://carecart.io/
- * @version 10.16
+ * @version 10.28
  *
  * Any unauthorized use and distribution of this and related files, is strictly forbidden.
  * In case of any inquiries, please contact here: https://carecart.io/contact-us/
@@ -671,6 +671,10 @@ scriptInjection("https://code.jquery.com/jquery-3.2.1.min.js", function () {
     let finalSelector = '';
     let masterSelector = '';
 
+    if (Shopify.shop == "b50db6-cc.myshopify.com") {
+        customSelector = $jq321(".product-info__buy-buttons");
+        finalSelector = customSelector[0];
+    }
     if (Shopify.shop == "ortho-confort.myshopify.com") {
         customSelector = $jq321(".btn--add-to-cart");
         finalSelector = customSelector[0];
