e37e56cacfe3a541fb481fcb23dcf09a82a0d869
YasirVaival <145327125+YasirVaival@users.noreply.github.com>
2024-10-15 13:00:16 +0500

    CTI-4081
    Widget is misplaced

--- visitor/custom-location-visitor.js
+++ visitor/custom-location-visitor.js
@@ -3,7 +3,7 @@
  * @author CareCart
  * @link https://apps.shopify.com/partners/care-cart
  * @link https://carecart.io/
- * @version 10.39
+ * @version 10.49
  *
  * Any unauthorized use and distribution of this and related files, is strictly forbidden.
  * In case of any inquiries, please contact here: https://carecart.io/contact-us/
@@ -853,6 +853,10 @@ scriptInjection("https://code.jquery.com/jquery-3.2.1.min.js", function () {
     let customSelctor = "";
     let finalSelector = "";
 
+    if (Shopify.shop == "naduvi-dev.myshopify.com") {
+        customSelctor = $jq321(".quantity_addToCart-wrap");
+        finalSelector = customSelctor[0];
+    }
     if (Shopify.shop == "gaya-astorina.myshopify.com") {
         $jq321(function() {
             // Fix for translating widget store based
