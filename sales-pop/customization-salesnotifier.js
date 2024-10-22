cdaf31144c4a6ce8fd06a843e4e8cad17e27c141
YasirVaival <145327125+YasirVaival@users.noreply.github.com>
2024-10-22 19:34:42 +0500

    Update customization-salesnotifier.js

--- sales-pop/customization-salesnotifier.js
+++ sales-pop/customization-salesnotifier.js
@@ -3,7 +3,7 @@
  * @author CareCart
  * @link https://apps.shopify.com/partners/care-cart
  * @link https://carecart.io/
- * @version v10.71
+ * @version v10.72
  * Any unauthorized use and distribution of this and related files, is strictly forbidden.
  * In case of any inquiries, please contact here: https://carecart.io/contact-us/
  */
@@ -3941,9 +3941,9 @@ scriptInjection("https://code.jquery.com/jquery-3.2.1.min.js", function () {
         var selectorTimer5 = $jq321("#shopify-section-product-template").find("form[action='/cart/add']").find("button[type='submit'],input[type='submit']").parent();
         var selectorTimer6 = $jq321("#shopify-section-product-template").find("form[action='/cart/add']");
 
-	 if (Shopify.shop == "9m12dq-mc.myshopify.com") {
-            masterSelector = $jq321("#shopify-block-adoric_bundles_bundle_NwDGrj");
-            finalSelector = masterSelector[0];
+        if (Shopify.shop == "9m12dq-mc.myshopify.com") {
+            	masterSelector = $jq321(".product__tax");
+        	finalSelector = masterSelector[0];
         }
 	if (Shopify.shop == "27089e.myshopify.com") {
 		masterSelector = $jq321(".sold-counter-content-box");
