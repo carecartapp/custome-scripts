25e1850ea176c5dfd3be50156137661943c4a59b
YasirVaival <145327125+YasirVaival@users.noreply.github.com>
2024-11-04 13:13:49 +0500

    CTI-4193 | CTI-4191
    CTI-4193
    Consent Box Layout Is Disturbed
    
    CTI-4191
    Page goes blank when wheel is closed

--- spinner/new_app_spinner.js
+++ spinner/new_app_spinner.js
@@ -3153,6 +3153,14 @@
                 //***************************** Store Specific Styling ***********************************************************
                 //***************************** Fix Text Positioning of Store in Spinner Pop-up **********************************
 
+		if (Shopify.shop == '5b7f6f-d3.myshopify.com') {
+                    carecartSpinnerJquery('body').append("<style type='text/css'>#wheelify-spin_a_sale_cc_store_front_module .checkbox{background: transparent;border: none;width: auto;height: auto;box-shadow: none;} .btn-submit-form{text-align: center;} .wheelify-signupContainer .checkbox input{margin-top: 4px;}</style>");       
+        	}
+		if (Shopify.shop == 'shopping-at-tiffanys.myshopify.com') {
+                    carecartSpinnerJquery(document).on('click', '#spin_a_sale_cc_store_front_module_close_button, #wheelify-spin-trigger-cc', function(event) {
+                        event.preventDefault();
+                    });
+                }
 		if (Shopify.shop == 'les-petits-monaco.myshopify.com') {
                     carecartSpinnerJquery('body').append("<style type='text/css'>.wheelText{fill: black; font-size: 30px !important; font-weight: 700 !important;}#wheelify-spin_a_sale_cc_store_front_module .wheelify-signupContainer p {color: black;}#wheelify-spin_a_sale_cc_store_front_module p.wheelify-text-description {color: black;}span#cc-spin-a-sale-consent-text {color: black !important;}#wheelify-spin-trigger-cc span {color: black;}#wheelify-spin_a_sale_cc_store_front_module .wheelify-signupContainer {margin-top: 70px !important;}#wheelify-spin_a_sale_cc_store_front_module .wheelify-loseContainer, #wheelify-spin_a_sale_cc_store_front_module .wheelify-winContainer {margin-top: 100px !important;}#wheelify-spin_a_sale_cc_store_front_module .wheelify-text-heading {color: black !important;}@media only screen and (max-width: 575px){#wheelify-spin_a_sale_cc_store_front_module .wheelify-signupContainer {margin-top: 40px !important;}#wheelify-spin_a_sale_cc_store_front_module .wheelify-wheelContainer {margin-top: -140px !important;}}</style>");    
         	}
