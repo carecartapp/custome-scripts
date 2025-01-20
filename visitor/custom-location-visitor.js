4092dbc1d6e2fe8c3315995e89b11d13cdb420a2
HafizMSubhan <hafiz.muhammad.subhan@vaival.com>
2025-01-20 13:27:38 +0500

    CTI-4614 | Widget placement below ATC on Product Pages

--- visitor/custom-location-visitor.js
+++ visitor/custom-location-visitor.js
@@ -867,6 +867,10 @@ scriptInjection("https://code.jquery.com/jquery-3.2.1.min.js", function () {
     let customSelctor = "";
     let finalSelector = "";
 
+    if (Shopify.shop == "bodylab-ch-prod2.myshopify.com") {
+        customSelctor = $jq321(".sold-out__container");
+        finalSelector = customSelctor[0];
+    }
     if (Shopify.shop == "aysey0-3e.myshopify.com") {
         customSelctor = $jq321(".price--show-badge");
         finalSelector = customSelctor[0];
